35f346a83db116828c0d97f432ca7829704e4946
jeongjihun <wlgns2234@naver.com>
2025-12-21 16:30:11 +0900

    feat: useRoute 를 활용한 경로 파라미터 받아오기

--- basic/src/views/member/MemberDetail.vue
+++ basic/src/views/member/MemberDetail.vue
@@ -3,7 +3,14 @@
 import {computed, inject} from "vue";
 import type {Member} from "@/interfaces/Member.ts";
 import {membersKey} from "@/keys/membersKey.ts";
+import {useRoute, useRouter} from "vue-router";
 
+// 1. useRoute() 를 통해 경로 파라미터 정보 가져오기
+const route = useRoute();
+const id = route.params.id;
+console.log("useRoute 를 통해 얻은 경로 파라미터 : " + id);
+
+// 2. props 를 통해 경로 파라미터 정보 가져오기.(router > index.ts 파일에 props 변환 코드를 작성해야한다.)
 const props = defineProps<{
   id: number
 }>();
