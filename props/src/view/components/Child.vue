3bfdd1c3a716f45e702c1a45f1bb6591156dec09
jeongjihun <wlgns2234@naver.com>
2025-12-28 14:38:49 +0900

    feat: 부모 -> 자식 데이터 전송(Props)

--- props/src/view/components/Child.vue
+++ props/src/view/components/Child.vue
@@ -1,10 +1,42 @@
 <script setup lang="ts">
 
+interface Props {
+  title: string
+  content: string
+  detail?: string
+}
+
+/**
+ *   1. 기본 사용
+ *   const props = defineProps<Props>()
+ */
+
+/**
+ *   2. default 값 사용
+ *
+ *   withDefaults(
+ *      defineProps<Props>(),
+ *      {
+ *          필수가 아닌 항목 Props 명: 기본값,
+ *          ...
+ *      }
+ *   )
+ */
+const defaultProps = withDefaults(
+    defineProps<Props>(),
+    {detail: "[undefined]"},
+)
+
 </script>
 
 <template>
   <div class="container">
     <h1>자식</h1>
+    <ul>
+      <li>{{ defaultProps.title }}</li>
+      <li>{{ defaultProps.content }}</li>
+      <li>{{ defaultProps.detail }}</li>
+    </ul>
   </div>
 </template>
 
--- props/src/view/components/Parents.vue
+++ props/src/view/components/Parents.vue
@@ -1,12 +1,26 @@
 <script setup lang="ts">
 
 import Child from "@/view/components/Child.vue";
+import {ref} from "vue";
+
+const propsTitle = ref("제목입니다.")
+const propsContent = ref("내용입니다.")
+
 </script>
 
 <template>
+  <!--
+  <자식 컴포넌트명
+    v-bind:Prop명="템플릿 변수명"
+    ...
+  />
+  -->
   <div class="container">
     <h1>부모</h1>
-    <Child></Child>
+    <Child
+      v-bind:title="propsTitle"
+      v-bind:content="propsContent"
+    />
   </div>
 </template>
 
